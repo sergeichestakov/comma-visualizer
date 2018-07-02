999120c06e008cfe33c5c740cbb10fd63e83b1a0
Sergei Chestakov <schestakov@ucdavis.edu>
2018-07-01 21:23:57 -0700

    Populate sqlite database

---
+++ populate.py
@@ -0,0 +1,38 @@
+import parse
+import sqlite3
+
+class Populator():
+    """
+    Class to interface with and populate the SQLite database
+    """
+
+    DB = 'db/trips.db'
+    def __init__(self, data):
+        self.conn = sqlite3.connect(self.DB)
+        self.cursor = self.conn.cursor()
+        self.data = data
+
+    def create_table(self):
+        sql = "CREATE TABLE IF NOT EXISTS trips (key TEXT, json TEXT)"
+        self.cursor.execute(sql)
+
+    def add_entry(self, date, json):
+        sql = "INSERT INTO trips VALUES (?,?)"
+        row = (date, json)
+        self.cursor.execute(sql, row)
+
+    def run(self):
+        self.create_table()
+
+        for date, arr in self.data.items():
+            for obj in arr:
+                self.add_entry(date, obj)
+        self.conn.close()
+
+def main():
+    trips = parse.generateJSON()
+    populator = Populator(trips)
+    populator.run()
+
+if __name__ == '__main__':
+    main()
