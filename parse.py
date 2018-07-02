42d5c34693adac98ef096790171bac9e26e2793f
Sergei Chestakov <schestakov@ucdavis.edu>
2018-07-01 21:22:24 -0700

    Add documentation and convert json to string

--- parse.py
+++ parse.py
@@ -2,6 +2,14 @@ import json
 import os
 
 def generateJSON():
+    """
+    Iterates through, parses, and reads every json file in trips directory
+
+    :returns:
+        dictionary keyed by date as a string of the form YYYYMMDD
+        that stores arrays of valid JSON strings containing the JSON object
+        stored in every file corresponding to that day
+    """
     DATA_DIR = './trips/'
     trips = {}
 
@@ -19,7 +27,7 @@ def generateJSON():
                 key = year + month + day
                 if key not in trips:
                     trips[key] = []
-                trips[key].append(jsonObj)
+                trips[key].append(json.dumps(jsonObj))
             except:
                 print('cannot open', filename)
 
